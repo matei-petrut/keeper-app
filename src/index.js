e14eb99203fd040acb6056e966681c9dc11ffe50
matei-petrut <petrutmatei98@gmail.com>
2020-07-29 11:17:17 +0300

    remove home signUp login files

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import Home from "./components/Home";
-import SignUp from "./components/SignUp";
-import { BrowserRouter, Route, Link, Switch } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Switch>
-      <Route component={Home} exact path="/" />
-      <Route component={App} path="/app" />
-      <Route component={SignUp} path="/signup" />
-    </Switch>
-  </BrowserRouter>
+  <App />
   , document.getElementById("root"));
