1fa1cc620598cfb91b544f1fdcb25cccdf46ba51
matei-petrut <petrutmatei98@gmail.com>
2020-07-29 14:50:12 +0300

    fix index.html

---
+++ build/precache-manifest.5f5bd9d7e1e90aba97fc17b31f91216d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bbb098c183b43920979f0798c93a219",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d73b96ad79f6ab16a066",
+    "url": "/static/js/2.1e1d4d77.chunk.js"
+  },
+  {
+    "revision": "1260a01290ac0c0454e9a7627323041c",
+    "url": "/static/js/2.1e1d4d77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c7f1a0a5eabcbe49c28",
+    "url": "/static/js/main.eeca10cc.chunk.js"
+  },
+  {
+    "revision": "272b2118ce1a3384d9bd",
+    "url": "/static/js/runtime-main.342c7561.js"
+  }
+]);
